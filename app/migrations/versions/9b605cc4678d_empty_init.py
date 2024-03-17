411d97d5253f3e4fed8b890bb8c107457a3a5d98
Kamenchu <iankamenchu@gmail.com>
2024-03-17 09:30:48 +0300

    completed code along

---
+++ app/migrations/versions/9b605cc4678d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 9b605cc4678d
+Revises: 
+Create Date: 2024-03-17 09:24:04.934009
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9b605cc4678d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
